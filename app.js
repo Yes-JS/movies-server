1c0bba1e8671d2ce24d712f095928bf9280c9458
evgeny-kirichuk <zhenyakirichuk@gmail.com>
2022-04-15 12:18:08 +0200

    cors policy update

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(helmet());
 app.use(helmet.hidePoweredBy());
 
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_FROM_URL);
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization');
 	res.setHeader('Access-Control-Allow-Credentials', "true");
 	next();
 });
